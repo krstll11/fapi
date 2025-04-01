3161db3a88105abb817f52202b731ed7a614d622
krstll11 <148008690+krstll11@users.noreply.github.com>
2025-04-01 11:42:18 +0500

    zadanie2

--- main.py
+++ main.py
@@ -1,32 +1,46 @@
 from typing import Union
 
 from fastapi import FastAPI, Query
-import random
-import math
+from validator import Item
 
 app = FastAPI()
+items=[{
+    "id":1,
+    "name":"laptop",
+    "price":1000,
+    "description":"This is a laptop"
+}]
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/items/")
+def find_items(name:Union[str,None]=None, min_price:Union[float,None]=None,max_price:Union[float,None]=None,limit:Union[int,None]=None):
+    yilded_count=1
+    for item in items:
+        if name and item["name"]!=name:
+            continue
+        if min_price and item["price"]<min_price:
+            continue
+        if max_price and item["price"]>max_price:
+            continue
+        if limit and yilded_count>limit:
+            break
+        yilded_count+=1
+        yield item
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-@app.get("/about_me")
-def display_about_me():
-    return {"name": "I am a student",
-            "group": "ISIT-323901"
-            }
-@app.get("/random_number")
-def display_random_number():
-    return {"random_number": random.randint(1, 100)}
-@app.post("/triangle")
-def s_display_triangle(a: int=Query(gt=0), b: int=Query(gt=0), c: int=Query(gt=0)):
-        if a+b<=c or a+c<=b or b+c<=a:
-            return {"error": "no triangle"}
-        else:
-            p=(a+b+c)/2
-            return {math.sqrt(p*(p-a)*(p-b)*(p-c))}
+def read_item(item_id:int)->Item:
+    for item in items:
+        if item["id"]==item_id:
+            return item
+
+
+@app.post("/items/")
+def create_item(item: Item)->Item:
+    item={
+    "id":len(items)+1,
+    "name":item.name,
+    "price":item.price,
+    "description":item.description
+}
+    items.append(item)
+    return item
